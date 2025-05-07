19f35b4753a6d897a73cb94b5bfb5b87725ddc8d
Ettienne600232 <600232@student.belgiumcampus.ac.za>
2025-05-07 20:27:56 +0000

    made some changes to the app.js file. Added the routes to the different pages. Added a data file for peoples information and event information, cleaned the files due to footer and header being in 2 places.

--- community-portal-prefilled/routes/pageRoutes.js
+++ community-portal-prefilled/routes/pageRoutes.js
@@ -1,21 +1,36 @@
 // routes/pageRoutes.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const aboutData = require("../data/about");
+const eventsData = require("../data/events");
+
+let contacts = [];
+
+router.get("/", (req, res) => {
+  res.render("pages/home", { events: eventsData });
 });
 
-router.get('/about', (req, res) => {
+router.get("/about", (req, res) => {
+  res.render("pages/about", { team: aboutData });
 });
 
-router.get('/events', (req, res) => {
+router.get("/events", (req, res) => {
+  res.render("pages/events", { events: eventsData });
 });
 
-router.get('/contact', (req, res) => {
+router.get("/contact", (req, res) => {
+  res.render("pages/contact");
 });
 
-router.get('/thankyou', (req, res) => {
+router.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+  contacts.push({ name, email, message });
+  res.redirect("/thankyou");
 });
 
+router.get("/thankyou", (req, res) => {
+  res.render("pages/thankyou");
+});
 module.exports = router;
