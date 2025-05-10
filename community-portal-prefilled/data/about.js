e1a7b3e7b27f457e22076fe1edf83230309b37ce
Ettienne600232 <600232@student.belgiumcampus.ac.za>
2025-05-10 21:48:36 +0000

    Moved the data files into the structure, fixed the data files to .js instead of .ejs, added import modules to about and events in data folder, Importad the about data to the about page.

---
+++ community-portal-prefilled/data/about.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { name: "Ettienne Janse Van Vuuren", role: "Programmer" },
+  { name: "BobSmith", role: "Data-Handler" },
+  { name: "Clara Lee", role: "Designer" },
+];
---
+++ community-portal-prefilled/data/events.js
@@ -0,0 +1,22 @@
+//ADD events to the file which could be called to the evebts page
+module.exports = [
+  {
+    title: "BC Graduation",
+    date: "2025-05-016",
+    location: "Belgium CampusPretoria",
+    image: "",
+  },
+  { title: "Youth Day", date: "2025-06-16", location: "Webinar", image: "" },
+  {
+    title: "Tech for Seniors Seminar",
+    date: "2025-07-12",
+    location: " Conference Room",
+    image: "",
+  },
+  {
+    title: "Youth Sports Festival",
+    date: "2025-07-08",
+    location: "Pretoria Campus",
+    image: "",
+  },
+];
