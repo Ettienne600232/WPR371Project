e49681be7dc0b952add280594d87d82b309abe98
Ettienne600232 <600232@student.belgiumcampus.ac.za>
2025-05-12 06:44:58 +0000

    fixed the Home page, images show, footer is ajusted for every page,also added a packground for each page

--- community-portal-prefilled/app.js
+++ community-portal-prefilled/app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const pageRoutes = require("./routes/pageRoutes");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
