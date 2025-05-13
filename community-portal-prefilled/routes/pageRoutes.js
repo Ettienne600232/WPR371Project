2eb2d9769eafbd4ebe5a622d597c9d808b8dca90
Ettienne600232 <600232@student.belgiumcampus.ac.za>
2025-05-13 15:09:17 +0000

    fixed the contact page, Information is saved to contact.json file in data.

--- community-portal-prefilled/routes/pageRoutes.js
+++ community-portal-prefilled/routes/pageRoutes.js
@@ -1,12 +1,18 @@
-// routes/pageRoutes.js
-
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 
 const aboutData = require("../data/about");
 const eventsData = require("../data/events");
 
+const contactFilePath = path.join(__dirname, "../data/contact.json");
+
 let contacts = [];
+if (fs.existsSync(contactFilePath)) {
+  const data = fs.readFileSync(contactFilePath, "utf-8");
+  contacts = JSON.parse(data);
+}
 
 router.get("/", (req, res) => {
   res.render("pages/home", { events: eventsData });
@@ -26,17 +32,27 @@ router.get("/contact", (req, res) => {
 
 router.post("/contact", (req, res) => {
   const { name, email, message } = req.body;
+
   if (!name || !email || !message) {
     return res.status(400).send("All fields are required.");
   }
-  const id = Date.now(); // to generate id
-  contacts.push({ id, name, email, message });
 
-  console.log("Contact saved:", { id, name, email, message });
-  res.redirect("/thankyou");
+  const id = Date.now();
+  const newContact = { id, name, email, message };
+  contacts.push(newContact);
+
+  fs.writeFile(contactFilePath, JSON.stringify(contacts, null, 2), (err) => {
+    if (err) {
+      console.error("Error saving contact:", err);
+      return res.status(500).send("Internal Server Error.");
+    }
+    console.log("Saved");
+    res.redirect("/thankyou");
+  });
 });
 
 router.get("/thankyou", (req, res) => {
   res.render("pages/thankyou");
 });
+
 module.exports = router;
